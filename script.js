baa1528937cccb1c36619824401f9b6efa560a53
dianemolina <dianemoli15@hotmail.com>
2024-04-24 19:38:38 -0300

    segunda carga-modificaciones

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function reproducir(){
 function pausar(){
     video.pause()
 }
+//  hoja juguemos
+
+const funcionInicio=(event)=>{
+    console.log("se inicio el arrastre del cohete");
+    console.dir(evento.target);
+ 
+
+    evento.dataTransfer.setData("Text",evento.target.src)
+}
